e743f23b9ae5b61abb265556b422533cec5661f4
SameerAli321 <138884959+SameerAli321@users.noreply.github.com>
2024-08-16 10:19:36 +0500

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,27 @@
+/*import React from 'React'
+import reactdom from 'reactdom'
+
+ function hello(){
+    return(<div> class= "App" 
+        <header class = "App - header">
+            <h1>
+                WELLCOME TOO THE REACT 
+            </h1>
+        </header>
+            
+        
+    </div>)
+ }
+
+ export default hello()*/
+
+
+ function getname(name){
+    return name ;
+
+ }
+ let a= true;
+ let b= true;
+
+ console.log(a && b)
+.form-group label {
